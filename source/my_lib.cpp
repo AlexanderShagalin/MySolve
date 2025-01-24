78ef8e4c2dcf42a8939271d9e9f90456e27d44a5
AlexanderShagalin <shagalinam.nemo@yandex.ru>
2025-01-24 11:12:11 +0400

    Add function my_nan

--- source/my_lib.cpp
+++ source/my_lib.cpp
@@ -1,8 +1,22 @@
+#include <vector>
+
 #include "my_lib.hpp"
 
+bool my_nan(double value)
+{
+    if(value < std::numeric_limits<double>::lowest())
+        return true;
+    if(value > std::numeric_limits<double>::max())
+        return true;
+    if(value != value)
+        return true;
+    return false;
+}
 
 double solve(double a, double b, double c)
 {
+
+
 
     return 0;
 }
--- tests/tst_test_1.cpp
+++ tests/tst_test_1.cpp
@@ -6,7 +6,31 @@
 
 using namespace testing;
 
-TEST(solve, test_1)
+TEST(my_nan, 1)
 {
-
+    ASSERT_THAT(my_nan(std::numeric_limits<double>::quiet_NaN()), Eq(true));
+}
+TEST(my_nan, 2)
+{
+    ASSERT_THAT(my_nan(std::numeric_limits<double>::signaling_NaN()), Eq(true));
+}
+TEST(my_nan, 3)
+{
+    ASSERT_THAT(my_nan(-std::numeric_limits<double>::epsilon()), Eq(false));
+}
+TEST(my_nan, 4)
+{
+    ASSERT_THAT(my_nan(std::numeric_limits<double>::infinity()), Eq(true));
+}
+TEST(my_nan, 5)
+{
+    ASSERT_THAT(my_nan(-std::numeric_limits<double>::infinity()), Eq(true));
+}
+TEST(my_nan, 6)
+{
+    ASSERT_THAT(my_nan(-300.676), Eq(false));
+}
+TEST(my_nan, 7)
+{
+    ASSERT_THAT(my_nan(100545513216800.676), Eq(false));
 }
